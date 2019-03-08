81dc6ee2184041e60be947243109e086de330ddf
saini-sanjay <sanjaynitj4066@gmail.com>
2019-03-08 22:35:08 +0530

    Updates

---
+++ precache-manifest.a662c658eb32120ddbf7a93243197fab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8de00767d32623e7a2d9",
+    "url": "/launch-demo/static/css/main.f55fde13.chunk.css"
+  },
+  {
+    "revision": "8de00767d32623e7a2d9",
+    "url": "/launch-demo/static/js/main.8de00767.chunk.js"
+  },
+  {
+    "revision": "2a74780450ae73d83006",
+    "url": "/launch-demo/static/js/runtime~main.2a747804.js"
+  },
+  {
+    "revision": "3791395ae6897930f09c",
+    "url": "/launch-demo/static/js/2.3791395a.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/launch-demo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f71f59724ee6046520ff621c6e607344",
+    "url": "/launch-demo/index.html"
+  }
+];
